afb822265d99f8f3a102ed72b5b55c54c7018bc9
subhashinivellaturi <22BQ1A47A6@vvit.net>
2025-02-12 21:51:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Navbar Scroll Effect
+window.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {
+        navbar.classList.add("scrolled");
+    } else {
+        navbar.classList.remove("scrolled");
+    }
+});
+
+// Hamburger Menu Toggle (Mobile)
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
